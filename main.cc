bd6bc134283916c12ff2d6723b0514ee02ce781a
Mona Lisa <troyappel@college.harvard.edu>
2020-03-27 13:45:47 -0700

    Various bugfixes, simplify strassen_s with wrapper function subSet

--- main.cc
+++ main.cc
@@ -2,25 +2,28 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h> 
+#include <fstream>
+#include <assert.h>
+#include <chrono>
 
-size_t CUTOFF = 10;
+size_t CUTOFF = 3;
 
 struct Matrix{
     size_t sz;
-    double* arr; 
+    long* arr; 
 
     Matrix(size_t sz) {
         this->sz = sz;
-        this->arr = new double[sz * sz];
+        this->arr = new long[sz * sz];
         this->clear();
     }
 
-    inline double& index(size_t x, size_t y) {
+    inline long& index(size_t x, size_t y) {
         return this->arr[sz*x + y];
     }
 
     inline void clear() {
-        memset(arr, 0, sz*sz*sizeof(double));
+        memset(arr, 0, sz*sz*sizeof(long));
     }
 
     ~Matrix() {
@@ -32,7 +35,7 @@ struct Matrix{
         for(int i = 0; i < sz; i++) {
             printf("   [");
             for(int j = 0; j < sz; j++) {
-                printf("%.2f,", this->index(j,i));
+                printf("%ld,", this->index(i,j));
             }
             printf("]\n");
         }
@@ -44,7 +47,7 @@ struct MatPak;
 
 MatPak make(Matrix* m,size_t s_x,size_t s_y, size_t e_x, size_t e_y);
 
-
+long nothing = 0;
 
 struct MatPak {
     Matrix* m;
@@ -54,12 +57,13 @@ struct MatPak {
     size_t e_x;
     size_t e_y;
 
-    double nothing;
+    bool padded_r;
+    bool padded_d;
 
-    bool padded;
-
-    inline double& index(size_t x, size_t y) {
-        if(padded && (x == e_x - s_x - 1 || y == e_y - s_y - 1)) {
+    inline long& index(size_t x, size_t y) {
+        if(padded_r && (x == e_x - s_x - 1)) {
+            return nothing;
+        } else if(padded_d && (y == e_y - s_y - 1)) {
             nothing = 0;
             return nothing;
         }
@@ -70,23 +74,45 @@ struct MatPak {
         return make(m, this->s_x + s_x, this->s_y + s_y, this->s_x + e_x, this->s_y + e_y);
     };
 
-};
+    inline MatPak subSet(size_t i, size_t j) {
+        size_t n = this->e_x - this->s_x;
+        size_t s_x0 = (i == 1) ? 0 : (n+1)/2;
+        size_t s_y0 = (j == 1) ? 0 : (n+1)/2;
 
-// Always returns an even-size MatPak, padded with 0 if bounds given are odd 
-MatPak make(Matrix* m,size_t s_x,size_t s_y, size_t e_x, size_t e_y) {
-    bool padded;
-    size_t n = e_x - e_y;
-    if (n & 1) {
-        e_x++;
-        e_y++;
-        padded = true;
-    } else {
-        padded = false;
+        size_t e_x0 = s_x0 + (n+1)/2;
+        size_t e_y0 = s_y0 + (n+1)/2;
+
+
+        printf("(%ld, %ld), (%ld, %ld)\n", s_x0, s_y0, e_x0, e_y0);
+        return subPak(s_x0, s_y0, e_x0, e_y0);
     }
 
-    return MatPak{m, s_x, s_y, e_x, e_y, padded};
+};
+
+// Always returns an even-size MatPak, padded with 0 if bounds given extend past edge
+MatPak make(Matrix* m,size_t s_x,size_t s_y, size_t e_x, size_t e_y) {
+    bool padded_r = false;
+    bool padded_d = false;
+    size_t n_x = e_x - s_x;
+    if (e_x > m->sz) {
+        // printf("%ul, %ul, %ul, %ul\n", );
+        // e_x++;
+        padded_r = true;
+    }
+
+    size_t n_y = e_y - s_y;
+    if (e_y > m->sz) {
+        // printf("%ul, %ul, %ul, %ul\n", );
+        // e_y++;
+        padded_d = true;
+    }
+
+
+    return MatPak{m, s_x, s_y, e_x, e_y, padded_r, padded_d};
 
 }
+
+// MatPak submat(MatPak)
 
 
 void mmult_s(MatPak a, MatPak b, MatPak res) {
@@ -120,31 +146,37 @@ void madd_s(MatPak a, MatPak b, MatPak res) {
     assert(a.e_y - a.s_y == b.e_y - b.s_y);
     assert(a.e_x - a.s_x == b.e_x - b.s_x);
 
+    assert(a.e_y - a.s_y == res.e_y - res.s_y);
+    assert(a.e_x - a.s_x == res.e_x - res.s_x);
+
+
+    size_t n = a.e_x - a.s_x;
+
     for(int i = 0; i < a.e_x - a.s_x; i++) {
         for (int j = 0; j < a.e_y - a.s_y; j++) {
-            res.m->index(i + res.s_x, j + res.s_y) 
-                = a.m->index(i + a.s_x, j + a.s_y)
-                + b.m->index(i + b.s_x, j + b.s_y);
+            res.index(i, j) 
+                = a.index(i, j)
+                + b.index(i, j);
         }
     }
 
 }
 
 void msub_s(MatPak a, MatPak b, MatPak res) {
-    assert(a.m->sz >= a.e_x);
-    assert(a.m->sz >= a.e_y);
+    // assert(a.m->sz >= a.e_x);
+    // assert(a.m->sz >= a.e_y);
 
-    assert(b.m->sz >= b.e_x);
-    assert(b.m->sz >= b.e_y);
+    // assert(b.m->sz >= b.e_x);
+    // assert(b.m->sz >= b.e_y);
 
     assert(a.e_y - a.s_y == b.e_y - b.s_y);
     assert(a.e_x - a.s_x == b.e_x - b.s_x);
 
     for(int i = 0; i < a.e_x - a.s_x; i++) {
         for (int j = 0; j < a.e_y - a.s_y; j++) {
-            res.m->index(i + res.s_x, j + res.s_y) 
-                = a.m->index(i + a.s_x, j + a.s_y)
-                - b.m->index(i + b.s_x, j + b.s_y);
+            res.index(i, j) 
+                = a.index(i, j)
+                - b.index(i, j);
         }
     }
 
@@ -155,7 +187,7 @@ void madd(Matrix* a, Matrix* b, Matrix* res) {
     madd_s(MatPak{a, 0, 0, a->sz, a->sz}, MatPak{b, 0, 0, b->sz, b->sz}, MatPak{res, 0, 0, res->sz, res->sz});
 }
 
-void cmult(Matrix* a, double c) {
+void cmult(Matrix* a, long c) {
     size_t n = a->sz;
 
     for (int i = 0; i < n; i++) {
@@ -174,7 +206,7 @@ void mmult_strassen(Matrix* a, Matrix* b, Matrix* res) {
 }
 
 void mmult_strassen_s(MatPak a, MatPak b, MatPak res) {
-    size_t n2 = (a.e_x - a.s_x) / 2;
+    size_t n2 = (a.e_x - a.s_x + 1) / 2;
     size_t n = a.e_x - a.s_x;
 
     if (n <= 1) {
@@ -198,55 +230,62 @@ void mmult_strassen_s(MatPak a, MatPak b, MatPak res) {
     Matrix* scratch1 = new Matrix(n2);
     Matrix* scratch2 = new Matrix(n2);
 
-    madd_s(a.subPak( 0, 0, n2, n2), a.subPak( n2, n2, n, n), MatPak{scratch1, 0, 0, n2, n2});
-    madd_s(b.subPak( 0, 0, n2, n2), b.subPak( n2, n2, n, n), MatPak{scratch2, 0, 0, n2, n2});
+    MatPak scratch1_p = make(scratch1, 0, 0, n2, n2);
+    MatPak scratch2_p = make(scratch2, 0, 0, n2, n2);
+
+    // std::cout << n << "hither" << n2 << "\n" << std::flush;
+
+    madd_s(a.subSet(1,1), a.subSet(2,2), scratch1_p);
+    madd_s(b.subSet(1,1), b.subSet(2,2), scratch2_p);
     mmult_strassen(scratch1, scratch2, M1);
 
 
-    madd_s(a.subPak( 0, n2, n2, n), a.subPak( n2, n2, n, n), MatPak{scratch1, 0, 0, n2, n2});
-    mmult_strassen_s(MatPak{scratch1, 0,0,n2,n2}, b.subPak(0,0,n2,n2), MatPak{M2, 0, 0, M2->sz, M2->sz});
+    madd_s(a.subSet(2,1), a.subSet(2,2), scratch1_p);
+    mmult_strassen_s(scratch1_p, b.subSet(1,1), make(M2, 0, 0, M2->sz, M2->sz));
 
-    msub_s(b.subPak( n2, 0, n, n2), b.subPak( n2, n2, n, n), MatPak{scratch1, 0, 0, n2, n2});
-    mmult_strassen_s(MatPak{scratch1, 0,0,n2,n2}, a.subPak(0,0,n2,n2), MatPak{M3, 0, 0, n2, n2});
+    msub_s(b.subSet(1,2), b.subSet(2,2), scratch1_p);
+    mmult_strassen_s(a.subSet(1,1), scratch1_p, make(M3, 0, 0, n2, n2));
 
-    msub_s(b.subPak( 0, n2, n2, n), b.subPak( 0, 0, n2, n2), MatPak{scratch1, 0, 0, n2, n2});
-    mmult_strassen_s(MatPak{scratch1, 0,0,n2,n2}, a.subPak(n2,n2,n,n), MatPak{M4, 0, 0, n2, n2});
+    msub_s(b.subSet(2, 1), b.subSet(1, 1), scratch1_p);
+    mmult_strassen_s(a.subSet(2,2), scratch1_p, make(M4, 0, 0, n2, n2));
 
-    madd_s(a.subPak( 0, 0, n2, n2), a.subPak( n2, 0, n, n2), MatPak{scratch1, 0, 0, n2, n2});
-    mmult_strassen_s(MatPak{scratch1, 0,0,n2,n2}, b.subPak(n2,n2,n,n), MatPak{M5, 0, 0, M5->sz, M5->sz});
+    madd_s(a.subSet(1,1),a.subSet(1,2), scratch1_p);
+    mmult_strassen_s(scratch1_p, b.subSet(2,2), make(M5, 0, 0, n2, n2));
 
-    msub_s(a.subPak( 0, n2, n2, n), a.subPak( 0,0,n2,n2), MatPak{scratch1, 0, 0, n2, n2});
-    madd_s(b.subPak( 0,0,n2,n2), b.subPak( n2,0,n,n2), MatPak{scratch2, 0, 0, n2, n2});
+    msub_s(a.subSet(2,1), a.subSet(1,1), scratch1_p);
+    madd_s(b.subSet(1,1), b.subSet(1,2), scratch2_p);
     mmult_strassen(scratch1, scratch2, M6);
 
-    msub_s(a.subPak( n2, 0, n, n2), a.subPak( n2,n2,n,n), MatPak{scratch1, 0, 0, n2, n2});
-    madd_s(b.subPak(  0, n2, n2, n), b.subPak( n2,n2,n,n), MatPak{scratch2, 0, 0, n2, n2});
+    msub_s(a.subSet(1,2), a.subSet(2,2), scratch1_p);
+    madd_s(b.subSet(2,1), b.subSet(2,2), scratch2_p);
     mmult_strassen(scratch1, scratch2, M7);
+
+    M7->print();
+
+    M1->print();
+    M2->print();
+    M3->print();
+    M4->print();
+    M5->print();
+    M6->print();
+    M7->print();
 
 
     // Put together result
+    // Have to be more careful here, to keep bounds right!
 
-    madd_s(MatPak{M1, 0,0,n2,n2}, MatPak{M4, 0,0,n2,n2}, MatPak{res.m, 0, 0, n2, n2});
-    msub_s(MatPak{res.m, 0, 0, n2, n2}, MatPak{M5, 0,0,n2,n2}, MatPak{res.m, 0, 0, n2, n2});
-    madd_s(MatPak{res.m, 0, 0, n2, n2}, MatPak{M7, 0,0,n2,n2}, MatPak{res.m, 0, 0, n2, n2});
+    madd_s(make(M1, 0,0,n2,n2), make(M4, 0,0,n2,n2), res.subSet(1,1));
+    msub_s(res.subSet(1,1), make(M5, 0,0,n2,n2), res.subSet(1,1));
+    madd_s(res.subSet(1,1), make(M7, 0,0,n2,n2), res.subSet(1,1));
 
-    madd_s(MatPak{M3, 0,0,n2,n2}, MatPak{M5, 0,0,n2,n2}, MatPak{res.m, n2, 0, n, n2});
+    madd_s(make(M3, 0,0,n2,n2), make(M5, 0,0,n2,n2), res.subSet(1,2));
 
-    madd_s(MatPak{M2, 0,0,n2,n2}, MatPak{M4, 0,0,n2,n2}, MatPak{res.m, 0,n2, n2, n});
+    madd_s(make(M2, 0,0,n2,n2), make(M4, 0,0,n2,n2), res.subSet(2,1));
 
 
-    msub_s(MatPak{M1, 0,0,n2,n2}, MatPak{M2, 0,0,n2,n2}, MatPak{res.m, n2, n2, n, n});
-    madd_s(MatPak{res.m, n2, n2, n, n}, MatPak{M3, 0,0,n2,n2}, MatPak{res.m, n2, n2, n, n});
-    madd_s(MatPak{res.m, n2, n2, n, n}, MatPak{M6, 0,0,n2,n2}, MatPak{res.m, n2, n2, n, n});
-
-    // M1->print();
-    // M2->print();
-    // M3->print();
-    // M4->print();
-    // M5->print();
-    // M6->print();
-    // M7->print();
-
+    msub_s(make(M1, 0,0,n2,n2), make(M2, 0,0,n2,n2), res.subSet(2,2));
+    madd_s(res.subSet(2,2), make(M3, 0,0,n2,n2), res.subSet(2,2));
+    madd_s(res.subSet(2,2), make(M6, 0,0,n2,n2), res.subSet(2,2));
 
     delete M1;
     delete M2;
@@ -261,27 +300,66 @@ void mmult_strassen_s(MatPak a, MatPak b, MatPak res) {
 
 }
 
-int main() {
-    Matrix* a = new Matrix(3);
-    a->index(0,1) = 1;
-    a->index(1,0) = 1;
-    a->index(2,2) = 1;
+bool are_equal(Matrix* a, Matrix* b) {
+    for (size_t i = 0; i < a->sz*a->sz; i++) {
+        if(a->arr[i] != b->arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char** argv) {
+
+    size_t dim = strtoul(argv[2],nullptr, 10);
+
+    std::ifstream infile; 
+
+    infile.open(argv[3]);
+
+    if (!infile) {
+        printf("File N/A\n");
+        exit(1);
+    }
+
+    Matrix* a = new Matrix(dim);
+    Matrix* b = new Matrix(dim);
+
+    for(size_t i = 0; i < dim * dim; i++) {
+        infile >> a->arr[i];
+    }
+
+    for(size_t i = 0; i < dim * dim; i++) {
+        infile >> b->arr[i];
+    }
+
+    infile.close();
+
+    // a->print();
+    // b->print();
 
 
-    a->print();
+    Matrix* res = new Matrix(dim);
 
-    Matrix* b = new Matrix(3);
-    b->index(0,0) = 2;
-    b->index(1,1) = 2;
-    b->index(2,2) = 2;
-    b->print();
+    printf("Multiplying standard...");
 
-
-    Matrix* res = new Matrix(3);
-
-    mmult_strassen(a,b,res);
-    res->print();
-
+    auto begin = std::chrono::high_resolution_clock::now();
     mmult(a,b,res);
-    res ->print();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "ms\n";
+
+    Matrix* res_s = new Matrix(dim);
+    printf("Multiplying Strassen...");
+
+    begin = std::chrono::high_resolution_clock::now();
+    mmult_strassen(a,b,res_s);
+    end = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << "ms\n";
+
+    res->print();
+    res_s->print();
+
+    
+
+    assert(are_equal(res, res_s));
 }
--- makefile
+++ makefile
@@ -1,10 +1,19 @@
 strassen: main.cc
-	g++ -o strassen main.cc -O3 -pthread
+	g++ -o strassen main.cc -O1 -pthread -g
 
 run: clean strassen exec
 
 clean:
-	rm -f randmst
+	rm -f strassen
+
+define make_input = 
+	#!/bin/bash
+	> input.txt ; 
+	awk -v n=1000 'BEGIN{srand(); for(i=1;i<=n;i++) print(int(rand()*10))}' > input.txt
+endef
 
 exec:
-	./strassen
+	$(make_input) ; 
+	./strassen 0 15 input.txt
+
+.ONESHELL:
